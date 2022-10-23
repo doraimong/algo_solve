daeb9e2ed416ab4910c31f8b159358e20127b46c
Park jonghyun <78489319+doraimong@users.noreply.github.com>
2022-10-23 14:20:55 +0900

    [Gold III] Title: 양팔저울, Time: 84 ms, Memory: 12588 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	static char res;
+	static int chooCnt, chooArr[];
+	static boolean dp[][];
+	
+	static void solve(int i, int w) {
+		
+		if(i > chooCnt || dp[i][w]) {
+			return;
+		}
+		
+		dp[i][w] = true;
+		
+		solve(i+1, w);
+		solve(i+1, w+chooArr[i]);
+		solve(i+1, Math.abs(w-chooArr[i]));
+	}
+	
+	public static void main(String[] args) throws Exception {
+	
+		//System.setIn(new FileInputStream("input.txt"));
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st; 
+		
+		chooCnt = Integer.parseInt(in.readLine());
+		chooArr = new int[31];
+		
+		dp = new boolean[31][15001];
+		
+		st = new StringTokenizer(in.readLine());
+		for(int i=0; i<chooCnt; i++)chooArr[i] = Integer.parseInt(st.nextToken());
+		
+		int ballCnt = Integer.parseInt(in.readLine());
+		int[] ballArr = new int[ballCnt];
+		
+		solve(0, 0);
+		
+		st = new StringTokenizer(in.readLine());
+		
+		for(int i=0; i<ballCnt; i++) {
+			int ballSize = Integer.parseInt(st.nextToken());
+			if(ballSize > 15000)System.out.print("N ");
+			else if(dp[chooCnt][ballSize])System.out.print("Y ");
+			else System.out.print("N ");
+		}
+	}
+}
