228b458134878e670b28ff88c6545027dae40684
Park jonghyun <78489319+doraimong@users.noreply.github.com>
2023-05-25 11:39:46 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,42 +1,40 @@
+//참고 https://dontdiethere.tistory.com/77
 #include <iostream>
 using namespace std;
 
-int load[1001] = { 0 }, load1[1001] = { 0 }, dp[1001] = { 0 }, dp1[1001] = { 0 };
+int main()
+{
+    int total;
+    cin>>total;
+    
+    int arr[total], dpLeft[total], dpRight[total];
 
-int main() {
-	int total;
-	cin >> total;
+    for(int i=0; i<total; i++){
+        cin >> arr[i];
+    }
 
-	//입력
-	for (int i = 1; i <= total; i++)
-	{
-		int get;
-		cin >> get;
-		load[i] = load1[total+1-i] = get;
+    //왼쪽에서 시작하는 최장 증가 부분수열
+    for(int i=0; i<total; i++){
+        dpLeft[i] = 1;
+        for(int j=0; j<i; j++){
+            if(arr[j] < arr[i])dpLeft[i] = max(dpLeft[i], dpLeft[j]+1);
+        }
+    }
+    
+    //오른쪽에서 시작하는 최장증가부분수열
+    for(int i=total-1; i>-1; i--){
+        dpRight[i] = 1;
+        for(int j=total-1; j>i; j--){
+            if(arr[i] > arr[j])dpRight[i] = max(dpRight[i], dpRight[j]+1);
+        }
+    }
+    
+    // 해당 인덱스의 좌,우 최장증가부분수열 값이 제일 큰 인덱스 선정
+    int res = 0;
+    for(int i=0; i<total; i++){
+        res = max(res, dpLeft[i] + dpRight[i]);
+    }
+    cout<<res-1;
 
-	}
-
-	//solve
-	int res = 0, res1 = 0;
-	for (int i = 1; i <= total; i++)
-	{
-		int top = 0, top1 = 0;
-		for (int ii = 1; ii < i; ii++)
-		{
-			if (load[i] > load[ii])top = max(top, dp[ii]);
-			if (load1[i] > load1[ii])top1 = max(top1, dp1[ii]);
-		}
-		dp[i] = top + 1;
-		dp1[i] = top1 + 1;
-		res = max(res, dp[i]);
-		res1 = max(res1, dp1[i]);
-	}
-	int maxres = 0;
-	for (int i = 1; i <= total; i++)
-	{
-		dp[i] += dp1[total+1-i];
-		maxres = max(maxres, dp[i]);
-	}
-	cout << maxres-1 << endl;
-	return 0;
+    return 0;
 }
