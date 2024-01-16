ce146fbad4e0274e9a7937b69ab211add8b9c3c4
Park jonghyun <78489319+doraimong@users.noreply.github.com>
2024-01-17 01:27:04 +0900

    Update 풍선 터트리기.py

--- 프로그래머스/Lv.3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/Lv.3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -13,31 +13,25 @@ ex>
 3. 0번과 오른쪽 최소값만 남는다. 여기서 무조건 오른쪽 최소값을 날릴 수 있다.([-1, 5] 라면 오른쪽이 크기 때문에, [5, -1]라면 작은 풍선 터뜨릴수 있기 때문)
 
 '''
+
 def solution(a):
     answer = 2
     left_min, right_min = [a[0]],[a[-1] for _ in range(len(a))]
-    back_idx = len(a)-1
     
     test = []
-    left_min_val, right_min_val = int(1e9), a[-1]
+    right_min_val = a[-1]
     for i in range(1,len(a)-1): # 0, -1인덱스 값 제외 / 좌,우 최소값 탐색
-        # i자리값은 치면 안된다. 다시 ㄱ
         if left_min[-1] > a[i-1]:
             left_min.append(a[i-1])
         else :
             left_min.append(left_min[-1])
-          
-        if right_min_val > a[-1-i+1] :
+        
+        if right_min_val > a[-1-i+1] : # 뒤에서부터 인덱스를 조절해서 최소값을 구함
             right_min[-1-i] = right_min_val = a[-1-i+1]
         else : 
             right_min[-1-i] = right_min_val
-        # if right_min[back_idx-i+1] > a[back_idx-i] :
-        #     right_min[back_idx-i] = a[back_idx-i]
-        # else : 
-        #     right_min[back_idx-i] = right_min[back_idx-i+1]
             
     
-    # print(left_min)
     for i in range(1,len(a)-1):# 해당 요소의 좌,우 최소값 중 하나라도 요소의 값보다 크면 해당 요소는 answer 추가
         if left_min[i] > a[i]:
             answer += 1
@@ -49,20 +43,4 @@ def solution(a):
             answer += 1
             continue
     
-    # print(left_min)
-    # print(right_min)
-    # print(test)
     return answer
-#  -16*, -92*, -71*, -68, -61, -33
-# 27, 
-    
-# -16,27,65,-92
-# -71
-# -92,-71,-68
-
-# -16*,27,65,-2,58,-92*,-71*,-68*,-61*,-33*
-# -16,27,65,-2,58,-92,-71,-68,-61,-33
-
-# -92*, -71*, -68*, -61*, -33*, -16*, -2, 27, 58, 65
-
-# -1* 9 -5*
