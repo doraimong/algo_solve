55e9fc13b713192b04c9a5996cab5a6de48de513
Park jonghyun <78489319+doraimong@users.noreply.github.com>
2024-03-19 18:08:17 +0900

    Update 요격 시스템.py

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -5,10 +5,10 @@
 def solution(targets):
     answer = 0
     targets = sorted(targets, key=lambda x : x[1])
-    s=e=0
+    e=0
     for t in targets:
         if e <= t[0]:
             answer += 1
-            s, e = t[0], t[1]
+            e = t[1]
             
     return answer
